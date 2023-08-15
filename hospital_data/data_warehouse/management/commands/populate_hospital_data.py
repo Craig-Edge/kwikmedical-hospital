fd88c123bd4ff53564e19bcb3d202ce16b515c58
Craig Gordon <craig.gordon@edt.zone>
2023-08-15 22:39:01 +0100

    retrieve all patient data API working

---
+++ hospital_data/data_warehouse/management/commands/populate_hospital_data.py
@@ -0,0 +1,48 @@
+import random
+from itertools import cycle
+from django.core.management.base import BaseCommand
+from data_warehouse.models import Hospital
+
+
+# List of hospital names
+hospital_names = [
+    "Starship Hospital", "Enterprise Medical Center", "Voyager General", "Defiant Healthcare",
+    "Galactica Clinic", "Millennium Health Center", "Nebula Medical", "Pegasus Hospital",
+    "Prometheus Medical Center", "Discovery Care", "Falcon Healthcare", "Serenity General",
+    "USS Hospital", "Atlantis Medical Center", "Eagle Health Services", "Phoenix Clinic",
+    "Rocinante Hospital", "Tardis Medical", "Stargate General", "Normandy Care"
+]
+
+# List of facilities
+FACILITIES = [
+    'ICU', 'A&E', 'Burn Unit', 'Operating Theatres', 'Neuro Surgery'
+]
+
+def generate_random_coordinates():
+    # Generate random latitude and longitude within the specified range
+    latitude = random.uniform(55.8, 56.1)  # Latitude range for central belt of Scotland
+    longitude = random.uniform(-3.9, -4.4)  # Longitude range for central belt of Scotland
+    return f"{latitude},{longitude}"  
+
+class Command(BaseCommand):
+    help = 'Populate the hospital database with sample data'
+
+    def handle(self, *args, **options):
+        # Delete existing hospital data (optional)
+        Hospital.objects.all().delete()
+
+        facility_cycle = cycle(FACILITIES)  # Create a cyclic iterator for facilities
+
+        # Loop to create hospitals
+        for hospital_name in hospital_names:
+            facility = next(facility_cycle)  # Get the next facility from the cycle
+            hospital_location = generate_random_coordinates()  # Generate random coordinates
+
+            # Create the hospital record
+            Hospital.objects.create(
+                hospital_name=hospital_name,
+                facilities=facility,
+                hospital_location=hospital_location
+            )
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated hospital data'))
---
+++ hospital_data/data_warehouse/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Hospital(models.Model):
+    FACILITIES_CHOICES = (
+        ('ICU', 'icu'),
+        ('A&E', 'a&e'),
+        ('Burn Unit', 'burn unit'),
+        ('Operating Theatres', 'operating theatres'),
+        ('Neuro Surgery', 'neuro surgery')
+    )
+    
+    hospital_name = models.CharField(max_length=255, blank=False, null=False)
+    hospital_location = models.CharField(max_length=100)
+    facilities = models.CharField(max_length=255)
+    # specialization = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
